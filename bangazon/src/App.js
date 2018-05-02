cf86161bb015fb10ebfda893c0b6dd53fd667dee
Ben Atkins <benjatkins13@gmail.com>
2018-05-02 13:52:36 -0500

    commit to change branch

--- bangazon/src/App.js
+++ bangazon/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserInfo from './db.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <UserInfo />
       </div>
     );
   }
--- bangazon/src/db.js
+++ bangazon/src/db.js
@@ -1,1 +1,15 @@
 import React from 'react';
+let faker = require('faker');
+
+class UserInfo extends Component {
+    fakeUser(){
+        let userImage = faker.image.image();
+        console.log(userImage);
+    }
+    render() {
+      return (
+        <div className="UserInfo">
+        </div>
+      );
+    }
+  }
