ee942c6cdd242c7b854a079eeba144ee645ce25f
Ben Atkins <benjatkins13@gmail.com>
2018-05-02 15:17:08 -0500

    push userdata  to jsonserver

--- bangazon/src/db.js
+++ bangazon/src/db.js
@@ -5,7 +5,7 @@ let faker = require('faker');
 export class UserInfo extends Component {
     componentWillMount(){
       let users = [];
-      for(let i=0;i<3;i++){
+      for(let i=0;i<7;i++){
         let uid = faker.random.number();
         let userFName = faker.name.firstName();
         let userLName = faker.name.lastName();
@@ -26,6 +26,15 @@ export class UserInfo extends Component {
       })
     }
       console.log(users);
+      fetch("http://localhost:3000/user",
+{
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify(users)
+})
     }
     render() {
       return (
