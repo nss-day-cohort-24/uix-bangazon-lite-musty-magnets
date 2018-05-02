fe71156966f789116af1c7aa2283ad6a14ec667b
Dan Watson <danwatson00@yahoo.com>
2018-05-02 15:04:26 -0500

    got form values

--- bangazon/src/components/Display.js
+++ bangazon/src/components/Display.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ProductSellForm from './ProductSellForm';
 
 class Display extends Component {
+
     render() {
         return (
             <div>
