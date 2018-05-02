5c04bf4b1d5ba2e03ea77ccee811ca57f049a44e
Amber Sharpe <amberjsharpe@gmail.com>
2018-05-02 14:03:46 -0500

    added router

--- bangazon/src/App.js
+++ bangazon/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Topnav from './components/Topnav';
+import Sidenav from './components/Sidenav';
+import Display from './components/Display';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+        <div className="App">
+          <BrowserRouter />
+              <Topnav />
+              <Sidenav />
+              <Display />
+          <BrowserRouter />
       </div>
     );
   }
---
+++ bangazon/src/components/Display.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Display extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+
+}
+
+export default Display;
