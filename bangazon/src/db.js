6d75d21caa4623a56501ad4e77b8aec97297767e
Ben Atkins <benjatkins13@gmail.com>
2018-05-02 14:53:54 -0500

    build function, using faker, to produce a small array of user objects

--- bangazon/src/db.js
+++ bangazon/src/db.js
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom';
 let faker = require('faker');
 
 export class UserInfo extends Component {
-    componentDidMount(){
+    componentWillMount(){
+      let users = [];
+      for(let i=0;i<3;i++){
+        let uid = faker.random.number();
+        let userFName = faker.name.firstName();
+        let userLName = faker.name.lastName();
+        let userAddress = faker.address.streetAddress();
+        let userPhone = faker.phone.phoneNumber();
+        let seller = faker.random.boolean();
         let userImage = faker.image.image();
-        console.log(userImage);
+
+      users.push({
+        "uid": uid,
+        "first_name": userFName,
+        "last_name": userLName,
+        "address": userAddress,
+        "phone": userPhone,
+        "seller": seller,
+        "image":userImage
+
+      })
+    }
+      console.log(users);
     }
     render() {
       return (
