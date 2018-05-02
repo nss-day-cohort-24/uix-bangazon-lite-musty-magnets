357f87919789a399ac7cf6621472e811005e9365
Dan Watson <danwatson00@yahoo.com>
2018-05-02 14:32:53 -0500

    got product sell form made

--- bangazon/src/App.js
+++ bangazon/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Display from './components/Display';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Display />
       </div>
     );
   }
---
+++ bangazon/src/components/Display.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ProductSellForm from './ProductSellForm';
+
+class Display extends Component {
+    render() {
+        return (
+            <div>
+                <ProductSellForm />
+            </div>
+        )
+    }
+
+}
+
+export default Display;
