e43fce2c55f942b6fa3469f729e0d96d425e0730
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-05-02 14:59:10 -0500

    Add functionality to UserInfo

--- bangazon/src/db.js
+++ bangazon/src/db.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 let faker = require('faker');
+let count = 0;
+
 
 export class UserInfo extends Component {
+  
+  
     componentDidMount(){
         let userImage = faker.image.image();
         console.log(userImage);
+      count++;
+        if (count === 2){
+
+          console.log("2");
+          // let fakeData = new XMLHttpRequest();
+          // fakeData.open("POST", "http://localhost:3000/order");
+          // fakeData.send();
+    
+          // fakeData.addEventListener("load", function(){
+    
+          //   var data = JSON.parse(this.responseText);
+          //   console.log("Data: ", data);
+        }
     }
 
-    
+    compon
     render() {
+      
       return (
         <div className="UserInfo">
         hey there
@@ -28,18 +46,29 @@ export class UserInfo extends Component {
 
 
 
-    
+
     componentWillMount(){
+      
+      count++;
+      if (count === 1){
+        let fakeData = new XMLHttpRequest();
+        fakeData.open("GET", "http://localhost:3000/order");
+        fakeData.send();
 
-    let fakeData = new XMLHttpRequest();
-    fakeData.open("GET", "http://localhost:3000/order");
-    fakeData.send();
+        fakeData.addEventListener("load", function(){
 
-    fakeData.addEventListener("load", function(){
-
-      var data = JSON.parse(this.responseText);
-      console.log("Data: ", data);
+          var data = JSON.parse(this.responseText);
+          console.log("Data: ", data);
     });
+    
+
+
+
+    
   } 
+
+
+
+}
 
 }
