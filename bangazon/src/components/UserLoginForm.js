eda69e8c2af204de0ce5709193d4c12034f1e145
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-05-03 15:41:54 -0500

    Merge

--- bangazon/src/components/UserLoginForm.js
+++ bangazon/src/components/UserLoginForm.js
@@ -3,11 +3,34 @@ import UserLoginFormTemplate from './UserLoginFormTemplate';
 
 class UserLoginForm extends React.Component {
 
+
+
     getUserValues() {
         let userEmail = document.getElementById('userEmail').value;
         let userPassword = document.getElementById('userPassword').value;
         console.log("user login values", userEmail, userPassword);
-    }
+
+
+        let existingUser = {
+            "useremail": userEmail,
+            "userpassword": userPassword
+          }
+        console.log("This is the existing user:", existingUser);
+    
+        // fetch("http://localhost:3000/user",
+        //     {
+        //         headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json'
+        //         },
+        //         method: "GET"
+        //     }).then(function(response)
+        //         {
+        //             console.log("Response", response);
+        //         });
+            }
+
+
 
     render() {
         return (
