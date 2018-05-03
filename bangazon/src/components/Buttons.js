50dfd0e15f749cc213223d564d2a1c9b8db71b0f
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-05-03 14:42:47 -0500

    package lock commit

--- bangazon/src/components/Buttons.js
+++ bangazon/src/components/Buttons.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './button.css';
 
 
 export default class Buttons extends React.Component {
     render() {
         return (
             <div>
-                <Button color="primary">primary</Button>{' '}
-                <Button color="secondary">secondary</Button>{' '}
-                <Button color="success">success</Button>{' '}
-                <Button color="info">info</Button>{' '}
-                <Button color="warning">warning</Button>{' '}
-                <Button color="danger">danger</Button>{' '}
-                <Button color="link">link</Button>
+                <Button className="btn-account-save">Save</Button>{' '}
+                <Button className="btn-sell-list">List Product</Button>{' '}
+                <Button className="btn-keep-shopping">Keep Shopping</Button>{' '}
+                <Button className="btn-account-cancel">Cancel</Button>{' '}
+                <Button className="btn-sell-cancel">Cancel</Button>{' '}
+                <Button className="btn-add-to-cart">Add to Cart</Button>{' '}
+                <Button className="btn-checkout">Checkout</Button>{' '}
+                <Button className="btn-place-order">Place Order</Button>{' '}
+                <Button className="btn-remove">Remove</Button>{' '}
+                <Button className="btn-edit">Edit</Button>
             </div>
         );
     }
