d2fa8bc49b2d25e8f8216a3f7fe0f290258b7fb4
Ben Atkins <benjatkins13@gmail.com>
2018-05-03 10:08:12 -0500

    debug

--- bangazon/src/App.js
+++ bangazon/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { UserInfo } from './db';
+import { UserInfo } from './components/db';
 
 
 import { BrowserRouter } from 'react-router-dom';
