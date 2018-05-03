99ea0c55d6e2083a5734464f0eda0e73e72f7fdc
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-05-03 09:25:41 -0500

    added home page component

--- bangazon/src/components/ProductHomePage.js
+++ bangazon/src/components/ProductHomePage.js
@@ -2,8 +2,50 @@ import React from 'react';
 
 let ProductHomePage = () => {
     return(
-        <div>
-            hi there bois
+        <div className="d-flex flex-wrap mt-5"> 
+            <h2>Welcome to Bangazon LTE, Your place for (insert great h1 tag here)</h2>
+            <div className="card mt-5 mr-2 ml-2" style={{width: 18 + 'rem'}}>
+                <img className="card-img-top" src="" alt="Card cap" />
+                <div className="card-body">
+                    <p className="card-text">mroduct</p>
+                    <p className="card-text">15.00</p>                    
+                </div>
+            </div>
+            <div className="card mt-5 mr-2 ml-2" style={{width: 18 + 'rem'}}>
+                <img className="card-img-top" src="" alt="Card cap" />
+                <div className="card-body">
+                    <p className="card-text">mroduct</p>
+                    <p className="card-text">15.00</p>                    
+                </div>
+            </div>
+            <div className="card mt-5 mr-2 ml-2" style={{width: 18 + 'rem'}}>
+                <img className="card-img-top" src="" alt="Card cap" />
+                <div className="card-body">
+                    <p className="card-text">mroduct</p>
+                    <p className="card-text">15.00</p>                    
+                </div>
+            </div>
+            <div className="card mt-5 mr-2 ml-2" style={{width: 18 + 'rem'}}>
+                <img className="card-img-top" src="" alt="Card cap" />
+                <div className="card-body">
+                    <p className="card-text">mroduct</p>
+                    <p className="card-text">15.00</p>                    
+                </div>
+            </div>
+            <div className="card mt-5 mr-2 ml-2" style={{width: 18 + 'rem'}}>
+                <img className="card-img-top" src="" alt="Card cap" />
+                <div className="card-body">
+                    <p className="card-text">mroduct</p>
+                    <p className="card-text">15.00</p>                    
+                </div>
+            </div>
+            <div className="card mt-5 mr-2 ml-2" style={{width: 18 + 'rem'}}>
+                <img className="card-img-top" src="" alt="Card cap" />
+                <div className="card-body">
+                    <p className="card-text">mroduct</p>
+                    <p className="card-text">15.00</p>                    
+                </div>
+            </div>
         </div>
     )
 }
