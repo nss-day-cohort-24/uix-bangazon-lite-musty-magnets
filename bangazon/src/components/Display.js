ee9ba78a3f8515fd3a509b053e14335d2e67c1a2
Phonethip <phonethip.liu@gmail.com>
2018-05-03 09:16:19 -0500

    made a few css styling on topnavbar, sidebar and display area

--- bangazon/src/components/Display.js
+++ bangazon/src/components/Display.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import ProductSellForm from './ProductSellForm';
 import { Route } from 'react-router-dom';
 import ProductHomePage from './ProductHomePage';
-
+import './display.css'
 
 class Display extends Component {
     render() {
         return(
-            <div>
+            <div className="display-main col-9">
                 <Route exact path='/' component={ProductHomePage} />
                 <Route path='/ProductsSellForm' component={ProductSellForm} />
             </div>
