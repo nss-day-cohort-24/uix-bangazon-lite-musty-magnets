61cc8f742c1e4a1d128d4e2e82360aa4f1ae8304
Sam Kimball <s.j.kimball@icloud.com>
2018-05-04 14:43:02 -0500

    Added first radio button

--- bangazon/src/components/ChoosePayment.js
+++ bangazon/src/components/ChoosePayment.js
@@ -7,7 +7,15 @@ export default function ChoosePayment() {
 			<div className="col-10 mx-auto choose_payment">
 				<div className="header">
 					<h1>Choose your payment options</h1>
-				</div>
+					<div class="input-group">
+					  <div class="input-group-prepend">
+					    <div class="input-group-text">
+					    <input type="radio" aria-label="Radio button for following text input" />
+					    </div>
+					  </div>
+					  <input type="text" class="form-control" aria-label="Text input with radio button" />
+						</div>
+					</div>
 
 				<div className="row">
 					<div className="col">
