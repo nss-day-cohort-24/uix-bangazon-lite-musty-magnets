8197b79c92c842e183fb8205c72ca3cbb0f23e7c
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-05-07 14:26:05 -0500

    Add coder notes

--- bangazon/src/components/CreateUserForm.js
+++ bangazon/src/components/CreateUserForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CreateUserFormTemplate from './CreateUserFormTemplate';
 let newuserobject = {};
+let newRegistration = false;
 
 export default class CreateUserForm extends React.Component {
 constructor(props){
@@ -27,38 +28,81 @@ getNewUserValues() {
     let userCC = document.getElementById('userCC').value;
     let userCRV = document.getElementById('userCRV').value;
     //excluded state, zipcode..
+    let count = 1;
 
-    newuserobject = {
-           
-        first_name : firstName,
-        last_name : lastName,
-        useremail: newUserEmail,
-        userpassword : newUserPass,
-        address : userAddress,
-        city : userCity,  
-        phone : userTel, 
-        cardnumber : userCC,
-        crv : userCRV
-
-    }
-    
-    console.log("create user form values", firstName, lastName, newUserEmail, newUserPass, userAddress, userCity, userState, userZip, userTel, userCC, userCRV);
-    console.log("Object to pass: ", JSON.stringify(newuserobject));
     fetch("http://localhost:3000/user",
     {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-        method: "POST",
-        body: JSON.stringify(newuserobject)
-
+        method: "GET"
     }).then((resp)=>{ 
-        console.log("Response!!!: ", resp);
-        }).catch((error)=>{
-            console.log("Error :", error);
-        }); 
+        resp.json().then(
+            (resolved) =>{
+                // for each element of the array
+                resolved.forEach((element, index)=>{
+                    // if the email includes the string
+                    try{
+                        if(element.email.includes(newUserEmail)){
+                        console.log("User found. Not a new address", element);
+                        newRegistration=false; //
+                    }// if the email doesn't include the string
+                    else if(!element.email.includes(newUserEmail)){
+                        console.log("Not a match", count);
+                        count++;
+                        console.log((resolved.length-1===index), count > 0);
+                        if((resolved.length-1===index) && count > 0)
+                        {   
+                            
+                            newRegistration = true;
+                            console.log("New registration.", newRegistration);
+                        }
+                        
+                    }}
+                    catch(e){
+                        console.log("It broke! What DID you DO???!?!");
+                    }    
+                });
+                
+            
+            newuserobject = {
+                id: count,
+                first_name : firstName,
+                last_name : lastName,
+                email: newUserEmail,
+                userpassword : newUserPass,
+                address : userAddress,
+                city : userCity,  
+                phone : userTel, 
+                cardnumber : userCC,
+                crv : userCRV
+                }
+            console.log("True?", newRegistration, "Object?", newuserobject); 
+
+            if(newRegistration){
+
+                    
+                    console.log("create user form values", firstName, lastName, newUserEmail, newUserPass, userAddress, userCity, userState, userZip, userTel, userCC, userCRV);
+                    console.log("Object to pass: ", JSON.stringify(newuserobject));
+                    fetch("http://localhost:3000/user",
+                    {
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                            },
+                        method: "POST",
+                        body: JSON.stringify(newuserobject)
+                        // Id field added to Db.json users
+                
+                    }).then((resp)=>{ 
+                        console.log("Response: ", resp.statusText,".", resp);
+                        }).catch((error)=>{
+                            console.log("Error :", error.statusText,".");;
+                        }); 
+            }
+        }
+        
+        )
+    });
 }
+
     
 
 
