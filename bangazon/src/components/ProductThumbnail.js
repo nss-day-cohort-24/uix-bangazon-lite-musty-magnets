17a80cbf44cf9e980c41a87237a6ac474aa86267
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-05-07 14:23:46 -0500

    made requested changes

--- bangazon/src/components/ProductThumbnail.js
+++ bangazon/src/components/ProductThumbnail.js
@@ -3,12 +3,12 @@ import "./productThumbnail.css"
 
 let ProductThumbnail = (props) => {
     return(
-        <div className="card mt-4 mr-2 ml-2 product-card" >
+        <div className="card mt-4 mr-2 ml-2 product-card justify-content-center text-center text-align-center" >
             <div className="productImageDiv">
                 <img className="card-img-top" src="https://img.etsystatic.com/il/17906a/1270947660/il_570xN.1270947660_p1ea.jpg?version=0" alt="Card cap" />
-                <h2 className="card-title">DINO PLANTER</h2>
-            </div>
-            <div className="card-body ">
+                </div>
+                <h2 className="card-title text-center">DINOSAUR PLANTER</h2>
+                <div className="card-body">
                 <div className="row align-items-center">
                     <p className="card-text col text-left">$15.00 </p>
                     <p className="card-text col text-right prod-qty">Qty 13</p>
