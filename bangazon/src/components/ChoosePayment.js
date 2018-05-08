d4fd8e403e3f79f2ddde5736c3fc43bc2ab49fa0
Sam Kimball <s.j.kimball@icloud.com>
2018-05-08 15:23:47 -0500

    Added Browser Router

--- bangazon/src/components/ChoosePayment.js
+++ bangazon/src/components/ChoosePayment.js
@@ -2,19 +2,28 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './ChoosePayment.css';
 
+import {
+	BrowserRouter,
+	Route
+} from 'react-router-dom';
+
 import PaymentOptions from './PaymentOptions.js';
 import PaymentCC from './PaymentCC.js';
 import PaymentConfirm from './PaymentConfirm.js'
 
 export default function ChoosePayment() {
 	return (
-		<div className="col-8 mx-auto main_body">
-			<h2 className="col-12 body-title"><span>Payment Information</span><button className="btn btn-light">Back</button></h2>
+		<BrowserRouter>
 
-				{/*<PaymentOptions />*/}
-				{/*<PaymentCC />*/}
-				<PaymentConfirm />
-			
-		</div>
-		)
+			<div className="col-8 mx-auto main_body">
+				<h2 className="col-12 body-title"><span>Payment Information</span><button className="btn btn-light">Back</button></h2>
+
+					<PaymentOptions />
+					<PaymentCC />
+					<PaymentConfirm />
+				
+			</div>
+
+		</BrowserRouter>
+	)
 }
