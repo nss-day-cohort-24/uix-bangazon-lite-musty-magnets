d0f0a6f97dd4757d9ee5876dcb619d1fd71e6e85
Sam Kimball <s.j.kimball@icloud.com>
2018-05-08 00:58:16 -0500

    Refactoring for routing

--- bangazon/src/components/ChoosePayment.js
+++ bangazon/src/components/ChoosePayment.js
@@ -2,29 +2,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './ChoosePayment.css';
 
+import PaymentOptions from './PaymentOptions.js';
+import PaymentCC from './PaymentCC.js';
+
 export default function ChoosePayment() {
 	return (
-			<div className="container mx-auto choose_payment">
-				<h1 className="col-2 bang-logo">BANGAZON</h1>
-				<hr />
+		<div className="col-8 mx-auto main_body">
+			<h2 className="col-12 body-title"><span>Payment Information</span><button className="btn btn-light">Back</button></h2>
 
-				<div className="col-8 mx-auto main_body">
-					<h2 className="col-12 body-title"><span>Payment Information</span><button className="btn btn-light">Back</button></h2>
-
-					<div className="col-12 payment_box">
-
-						<button className="btn btn-secondary disabled">Apple Pay</button>
-						<p>OR</p>
-						<button className="btn btn-secondary">Credit Card</button>
-						<button className="btn btn-secondary disabled">PayPal</button>						
-
-					</div>
-
-				</div>
-
-				<hr />
-				<p className="copyright">©Bangazon Worldwide 2018</p>
-			</div>
-
+				{/*<PaymentOptions />*/}
+				<PaymentCC />
+			
+		</div>
 		)
 }
