3a394bf0120d6b9a242db061dea8765cd995f063
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-05-08 17:01:50 -0500

    consistent image thumbnail achieved (almost)--thumbnail title height adjusted to keep price from cresting and troughing

--- bangazon/src/components/CategoryProdTemplate.js
+++ bangazon/src/components/CategoryProdTemplate.js
@@ -29,7 +29,7 @@ let CategoryProdTemplate = (props) => {
     <div key={index} className="card mt-4 mr-2 ml-2 product-card" >
             <img className="card-img-top" src={product.image} alt="Card cap" />
 
-            <h2 className="card-title text-center">{product.name}</h2>
+            <h2 className="card-title d-flex align-items-center justify-content-center text-center">{product.name}</h2>
             <div className="card-body ">
                 <div className="row">
                     <p className="card-text col text-left">${product.price}</p>
