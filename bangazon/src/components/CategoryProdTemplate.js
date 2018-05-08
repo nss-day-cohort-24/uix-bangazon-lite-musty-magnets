7badd78803b3caa7db2789b893b33a124efc583b
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-05-08 10:35:16 -0500

    product template card styled

--- bangazon/src/components/CategoryProdTemplate.js
+++ bangazon/src/components/CategoryProdTemplate.js
@@ -7,17 +7,17 @@ let CategoryProdTemplate = (props) => {
     const categoryItems = props.data.map((product, index) =>
     <div key={index} className="card mt-4 mr-2 ml-2 product-card" >
             <img className="card-img-top" src={product.image} alt="Card cap" />
-            <h2 className="card-title">{product.name}</h2>
-            <div className="card-body ">
-                <div className="row">
-                    <p className="card-text col text-left">{product.price}</p>
-                    <p className="card-text col text-right prod-qty">{product.quantity}</p>
+            <h2 className="card-title text-center">{product.name}</h2>
+            <div className="card-body d-flex align-items-end justify-content-between">
+                <div className="d-flex align-items-end justify-content-between">
+                    <p className="card-text col">${product.price}</p>
+                    <p className="card-text col prod-qty">qty {product.quantity}</p>
                 </div>         
             </div>
     </div>
     )
     return(
-        <div>
+        <div className="d-flex flex-wrap">
             {categoryItems}
         </div>
     )
