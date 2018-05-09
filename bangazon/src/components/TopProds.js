966ec6d58ef43fbe95d9341327c1c529b9d76375
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-05-09 00:35:01 -0500

    fixed a bug in the sell form

--- bangazon/src/components/TopProds.js
+++ bangazon/src/components/TopProds.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import ProductDetail from './ProductDetail';
 
 
 let TopProds = (props) => {
     const products = props.data.slice(0,3);
     const topProds = products.map((product, index) =>
         <div key={index} className="">
-            <small>{product.name}</small>
+            <ProductDetail 
+                buttonLabel={<small>{product.name}</small>}
+                image={product.image}
+                price={product.price}
+                quantity={product.quantity}
+                name={product.name}
+                category={product.category}
+                description={product.description}
+                id={product.id}/>
         </div>
    )
     return(
