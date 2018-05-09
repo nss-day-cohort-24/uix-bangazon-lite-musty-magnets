f8d2dd62f6f8b5064c69fc657f4e60821e54fd54
Phonethip <phonethip.liu@gmail.com>
2018-05-09 14:46:13 -0500

    searchbar function works now working on diplay of search result

--- bangazon/src/components/SearchBar.js
+++ bangazon/src/components/SearchBar.js
@@ -4,7 +4,6 @@ import  ProductThumbnail from './ProductThumbnail';
 import './topnav.css';
 import Display from './Display';
 class SearchBar extends Component {
-   
    constructor(props) {
     super(props);
     this.state = {
@@ -18,25 +17,44 @@ class SearchBar extends Component {
    getProduct = (taco) => {
     console.log ("what is taco in getProduct?", taco);
 
-        fetch(`http://localhost:3000/product?=${taco}`)
+        fetch(`http://localhost:3000/product?q=${taco}`)
                 .then(response => response.json())
                 .then((data) => {
                     console.log ("what data do we have?", data);
-                 this.setState({
-                     productArray: data
-                 })
-            });
-            // DisplaySearch();
-            // console.log ("what is in productArray?", this.state);
+                    this.setState({
+                        productArray: data
+                    },  
+                 ReturnProductItems)
+                    console.log ("what is in productArray?", this.state);
+                })
     }     
 
         /* Event handler for search entry */
     handleInputChange = (e) => {
         // console.log ("what is e?", e.charCode);
-        if (e.charCode === 13) {
+        if (e.charCode === 13 && e.target.value !=="") {
             this.getProduct(e.target.value);
         }
     }
+
+
+    ReturnProductItems = (props) => {
+        const ProductItems = props.productArray.map((product, index) => {
+        
+            <div key={index} >
+
+            <ProductThumbnail 
+                data = {this.state.productArray}
+                image = {product.image}
+                name = {product.name}
+                price = {product.price}
+                quantity = {product.quantity}
+            />
+                
+            </div>
+            
+               })
+            }
     
     render() {
 
@@ -46,10 +64,10 @@ class SearchBar extends Component {
                <Input 
                className="Search-bar" type="text" onKeyPress={this.handleInputChange} id="search" placeholder="Search..." /> 
                 
-                {/* <Display /> */}
-                {/* <div className="d-flex flex-wrap">
+                <Display /> 
+                 <div className="d-flex flex-wrap">
                     { ReturnProductItems }
-                </div> */}
+                </div>
                 
             </div>
         )
